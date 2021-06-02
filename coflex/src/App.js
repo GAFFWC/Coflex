3b036eba8f3e0936c6e58a56a532629e618e01b6
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-02 21:46:11 +0900

    navigation

--- coflex/src/App.js
+++ coflex/src/App.js
@@ -1,53 +1,16 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./components/Movie";
-import "./App.css";
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: []
-    };
-
-    getMovies = async () => {
-        const {
-            data: {
-                data: { movies }
-            }
-        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-
-        this.setState({ isLoading: false, movies: movies });
-    };
-    componentDidMount = () => {
-        this.getMovies();
-    };
-    render = () => {
-        const { isLoading, movies } = this.state;
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                        <span className="loader__text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="movies">
-                        {movies.map((movie) => {
-                            return (
-                                <Movie
-                                    key={movie.id}
-                                    id={movie.id}
-                                    year={movie.year}
-                                    title={movie.title}
-                                    summary={movie.summary}
-                                    poster={movie.medium_cover_image}
-                                    genres={movie.genres}
-                                />
-                            );
-                        })}
-                    </div>
-                )}
-            </section>
-        );
-    };
-}
+import { BrowserRouter, HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import About from "./routes/About";
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navigation />
+            <Route path="/" component={Home} exact={true} />
+            <Route path="/about" component={About} />
+        </BrowserRouter>
+    );
+};
 
 export default App;
